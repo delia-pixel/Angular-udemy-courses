1694d8810ab1b9821a6fb4708606d77a8a289b5e
delia-pixel <tpatriciareinedia@gmail.com>
2023-07-27 13:13:24 +0100

    attribute directives

--- src/app/directives/highlight.directive.ts
+++ src/app/directives/highlight.directive.ts
@@ -1,17 +1,31 @@
-import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
-  selector: '[appHighlight]'
+  selector: '[appHighlight]',
 })
-export class HighlightDirective implements OnInit{
+export class HighlightDirective implements OnInit {
+  constructor(private elementRef: ElementRef, private renderer: Renderer2) {}
 
-  constructor(private elementRef: ElementRef, private renderer: Renderer2) { }
-  
-  ngOnInit() { 
+  @HostBinding('style.color') color: string = 'black';
+
+  @HostListener('mouseenter') mouseover(eventData: Event) {
     this.renderer.setStyle(
       this.elementRef.nativeElement,
       'background-color',
-      '#505AE9',);
+      '#505AE9'
+    );
+    this.color = 'white';
   }
 
+  @HostListener('mouseleave') mouseleave(eventData: Event) {
+    this.renderer.setStyle(
+      this.elementRef.nativeElement,
+      'background-color',
+      'transparent'
+    );
+    this.color = 'black';
+  }
+
+  @HostListener('mouseleave')
+  ngOnInit() {}
 }
