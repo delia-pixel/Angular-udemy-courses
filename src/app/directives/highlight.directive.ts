aee4f84c4da4824bc42e07a6aefb308758f60e2b
delia-pixel <tpatriciareinedia@gmail.com>
2023-07-27 13:14:36 +0100

    attribute directives

--- src/app/directives/highlight.directive.ts
+++ src/app/directives/highlight.directive.ts
@@ -1,31 +1,40 @@
-import { Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, Input, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]',
 })
 export class HighlightDirective implements OnInit {
   constructor(private elementRef: ElementRef, private renderer: Renderer2) {}
+  @Input('defaultColor') defaultColor: string = '#505AE9';
+  @Input() hoverColor: string = 'white';
 
-  @HostBinding('style.color') color: string = 'black';
+  @HostBinding('style.color') color: string = this.defaultColor;
 
   @HostListener('mouseenter') mouseover(eventData: Event) {
-    this.renderer.setStyle(
-      this.elementRef.nativeElement,
-      'background-color',
-      '#505AE9'
-    );
-    this.color = 'white';
+    // this.renderer.setStyle(
+    //   this.elementRef.nativeElement,
+    //   'background-color',
+    //   '#505AE9'
+    // );
+    this.color = this.defaultColor;
   }
 
   @HostListener('mouseleave') mouseleave(eventData: Event) {
-    this.renderer.setStyle(
-      this.elementRef.nativeElement,
-      'background-color',
-      'transparent'
-    );
-    this.color = 'black';
+    // this.renderer.setStyle(
+    //   this.elementRef.nativeElement,
+    //   'background-color',
+    //   'transparent'
+    // );
+    this.color = this.hoverColor;
   }
 
   @HostListener('mouseleave')
-  ngOnInit() {}
+  ngOnInit() {
+    // this.renderer.setStyle(
+    //   this.elementRef.nativeElement,
+    //   'background-color',
+    //   '#505AE9'
+    // );
+    this.color = this.defaultColor;
+  }
 }
